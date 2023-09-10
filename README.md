346ca3943d63cdb2b36d7384a788c92e481ff75f
Fernando <jose.huarezr@gmail.com>
2023-09-10 11:46:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# desarrollo-ios
+Isil Desarrollo iOS 2023-2
---
+++ pruebas/pruebas/pruebasApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebasApp.swift
+//  pruebas
+//
+//  Created by Xindy Alarcon on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
