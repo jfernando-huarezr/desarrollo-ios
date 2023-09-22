b258bd0e73b13f154809d6f37a229f9c1194c21e
Fernando <jose.huarezr@gmail.com>
2023-09-21 19:53:07 -0500

    nuevo proyecto clase 4

---
+++ navigation-clase-4/navigation-clase-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigation-clase-4
+//
+//  Created by Xindy Alarcon on 21/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
